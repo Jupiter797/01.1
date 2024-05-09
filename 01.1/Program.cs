a865edd7a6c8b9980edfaa46d38a7b33f68e0b4b
Serhii Pypa <jupiters797@gmail.com>
2024-05-09 13:38:09 +0300

    Добавьте файлы проекта.

---
+++ 01.1/Program.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _01._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string yearOfBirth = Interaction.InputBox
+                ("Рік вашого народження?",
+                "Рік народження");
+
+            DateTime now = DateTime.Now;
+
+            int age = Convert.ToInt32(yearOfBirth);
+
+            int years = now.Year - age;
+
+            MessageBox.Show($"Ваш вік: {years} ", "Вік", 
+                MessageBoxButtons.OK, 
+                MessageBoxIcon.Information);
+        }
+    }
+}
---
+++ 01.2/Program.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _01._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string myAge = Interaction.InputBox
+                ("Ваш вік?",
+                "Вік");
+
+            DateTime now = DateTime.Now;
+
+            int age = Convert.ToInt32(myAge);
+
+            int yearOfBirth = now.Year - age;
+
+            MessageBox.Show($"Рік вашого народження: {yearOfBirth}", "Рік", 
+                MessageBoxButtons.OK, 
+                MessageBoxIcon.Information);
+        }
+    }
+}
